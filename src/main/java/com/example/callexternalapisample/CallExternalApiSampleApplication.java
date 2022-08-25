7c52312e44e1ba60526d2826bdc5b4238536280f
kenwoo9y <ei.1px.9.01@gmail.com>
2022-08-25 23:07:15 +0900

    first commit

---
+++ src/main/java/com/example/callexternalapisample/CallExternalApiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.callexternalapisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallExternalApiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallExternalApiSampleApplication.class, args);
+	}
+
+}
